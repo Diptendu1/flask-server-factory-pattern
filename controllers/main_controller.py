768287fcd7e41d6e91c551915ea37a71ab72ed33
Diptendu1 <diptendu.chakraborty.2012@gmail.com>
2021-08-03 08:18:56 +0530

    Async implementation

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,2 +1,14 @@
+import requests
+import logging
+
+logger = logging.getLogger("app.access")
+
 def first():
+    logger.info("HERE")
     return "first API"
+
+
+async def get_json(url):
+    async with requests.get(url) as response:
+        assert response.status == 200
+        return await response.json()
--- views/app_routes.py
+++ views/app_routes.py
@@ -2,11 +2,16 @@ from . import routes
 from controllers import main_controller
 
 
-@routes.route("/")
+@routes.route("/", methods=['GET'])
 def index():
     return "Base API"
 
 
-@routes.route("/api/v1/first")
+@routes.route("/api/v1/first", methods=['GET'])
 def first():
     return main_controller.first()
+
+
+@routes.route("/api/v1/geturlres", methods=['GET'])
+async def get_url_res():
+    return main_controller.get_json("https://api.publicapis.org/entries")
