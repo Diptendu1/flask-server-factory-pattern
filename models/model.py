98d482370180aa29a18aa36ac337ae3454b8634d
Diptendu1 <diptendu.chakraborty.2012@gmail.com>
2021-08-03 22:38:26 +0530

    model creation commit

---
+++ models/model.py
@@ -0,0 +1,36 @@
+from .core import db
+from sqlalchemy import func
+from flask_security import (
+    SQLAlchemyUserDatastore, UserMixin, RoleMixin)
+
+
+class UserRole(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    created_on = db.Column(db.DateTime(), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    created_on = db.Column(db.DateTime(), default=func.now())
+    name = db.Column(db.Unicode(100))
+    email = db.Column(db.Unicode(100), unique=True)
+    phone = db.Column(db.Unicode(30), unique=True)
+    password = db.Column(db.String(200))
+    active = db.Column(db.Boolean, default=False)
+    roles = db.relationship(
+        "Role", secondary=UserRole.__table__)
+
+
+class Role(db.Model, RoleMixin):
+
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    created_on = db.Column(db.DateTime(), default=func.now())
+    name = db.Column(db.String(30), unique=True)
+    description = db.Column(db.String(200))
+
+    users = db.relationship(
+        "User", secondary=UserRole.__table__)
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
